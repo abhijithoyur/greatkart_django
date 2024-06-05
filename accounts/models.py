6e003d7eb6a866ce7667b2d1c9c09d06d14b078c
Abhi <abhijithlaloyur@gmail.com>
2024-06-05 12:02:18 +0530

    first commit

---
+++ accounts/models.py
@@ -0,0 +1,77 @@
+from django.utils.translation import gettext_lazy as _
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+# Custom superuser model manager.
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('User must have an email address')
+
+        if not username:
+            raise ValueError('User must have a username')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username =username,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_admin', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+            is_admin = True,
+            is_active = True,
+            is_staff = True,
+            is_super_admin = True
+        )
+        print("User created successfully")
+        user.save(using=self._db)
+        return user
+
+# Custom user model.
+class Account(AbstractBaseUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=50)
+
+    # Required fields.
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_super_admin = models.BooleanField(default=False)
+
+    # Customize Django's default user model to enable login using email and password.
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name','phone_number']
+
+    objects = MyAccountManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, add_label):
+        return True
---
+++ store/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product
+from category.models import Category
+# Create your views here.
+def store(request,category_slug=None):
+    category = None
+    products = None
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category = categories, is_available = True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available = True)
+        product_count = products.count()
+    context = {
+        'products' : products,
+        'product_count' : product_count
+    }
+    return render(request,'store/store.html', context)
+
+
+def product_detail(request,category_slug,product_slug):
+    try:
+        product = get_object_or_404(Product,category__slug=category_slug, slug=product_slug, is_available=True)
+        category = get_object_or_404(Category, slug=category_slug)
+    except Exception as e:
+        raise e
+    context ={
+        'product' : product,
+        'category' : category
+    }
+    return render(request,'store/product_detail.html', context)
+
+
+
